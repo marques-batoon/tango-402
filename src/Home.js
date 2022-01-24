8c98a72cbc1ef4a2f920b463f6a156568ce44a45
Marques Batoon <marquesb@Marquess-MacBook-Pro.local>
2022-01-23 19:51:09 -1000

    added all kanji in unit 1-2 again

--- src/Home.js
+++ src/Home.js
@@ -45,8 +45,8 @@ class Home extends React.Component {
                 
                 <div className="homeButtonContainer">
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-tango-2" className="btn from-left">U1_2<br></br>単語</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-kaku-2" className="btn from-left">U1_2<br></br>書き漢字</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="u1-yomu-2" className="btn from-right">U1_2<br></br>読み漢字</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-kaku-2" className="btn from-left">U1_2<br></br>書き漢字</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="u1-yomu-2" className="btn from-right">U1_2<br></br>読み漢字</button></Link>
                 </div>
 
                 <div className="homeButtonContainer">
