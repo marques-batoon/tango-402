c958b2cecf6a47be2c9e5b964f7d565e2772fcf3
Marques Batoon <marquesb@dhcp-168-105-86-249.wireless.manoa.hawaii.edu>
2022-02-07 09:22:10 -1000

    all good. tango 2-1 added to all. site working

--- src/Yomu.js
+++ src/Yomu.js
@@ -3,6 +3,7 @@ import kanji from './kanji';
 import './kanji.css';
 import unit1_1 from './chapters/unit1_1';
 import unit1_2 from './chapters/unit1_2';
+import unit2_1 from './chapters/unit2_1';
 
 class Yomu extends React.Component {
     constructor(props) {
@@ -137,6 +138,51 @@ class Yomu extends React.Component {
                 characters: Object.keys(unit1_2.yomerebaii),
                 set: "yomu",
                 setName: "U1_2 読み方を覚える漢字",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            };
+        }
+        else if (test==='?set=u2-tango-1'){
+            this.state={
+                characters: Object.keys(unit2_1.tango),
+                set: "tango",
+                setName: "U2 単語 1",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            }
+        }
+        else if (test==='?set=u2-kaku-1') {
+            this.state = {
+                characters: Object.keys(unit2_1.kaku),
+                set: "kaku",
+                setName: "U2_1 書き方を覚える漢字",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            };
+        }
+        else if (test==='?set=u2-yomu-1') {
+            this.state = {
+                characters: Object.keys(unit2_1.yomerebaii),
+                set: "yomu",
+                setName: "U2_1 読み方を覚える漢字",
                 num: 0,
                 shuffleHide: "",
                 yomikataButton: "",
