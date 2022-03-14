83389662a0cfca6c34a507bbdf8a30f93a550ea1
Marques Batoon <marquesb@Marquess-MacBook-Pro.local>
2022-03-13 17:59:12 -1000

    added buttons and setup templates for 3-2 tango

--- src/Yomu.js
+++ src/Yomu.js
@@ -8,6 +8,8 @@ import unit2_2 from './chapters/unit2_2';
 import unit2_2_sankou from './chapters/unit2_2_sankou';
 import unit3_1 from './chapters/unit3_1';
 import unit3_1_sankou from './chapters/unit3_1_sankou';
+import unit3_2 from './chapters/unit3_2';
+import unit3_2_sankou from './chapters/unit3_2_sankou';
 
 class Yomu extends React.Component {
     constructor(props) {
@@ -316,6 +318,66 @@ class Yomu extends React.Component {
                 prevDis: true,
                 nextDis: false,
             };
+        }
+        else if (test==='?set=u3-tango-2'){
+            this.state={
+                characters: Object.keys(unit3_2.tango),
+                set: "tango",
+                setName: "U3 単語 2",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            }
+        }
+        else if (test==='?set=u3-kaku-2') {
+            this.state = {
+                characters: Object.keys(unit3_2.kaku),
+                set: "kaku",
+                setName: "U3_2 書き方を覚える漢字",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            };
+        }
+        else if (test==='?set=u3-yomu-2') {
+            this.state = {
+                characters: Object.keys(unit3_2.yomerebaii),
+                set: "yomu",
+                setName: "U3_2 読み方を覚える漢字",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            };
+        }
+        else if (test==='?set=u3-tango-2_sankou'){
+            this.state={
+                characters: Object.keys(unit3_2_sankou.tango),
+                set: "tango",
+                setName: "U3 参考にする単語 2",
+                num: 0,
+                shuffleHide: "",
+                yomikataButton: "",
+                yomikataClass: "d-none",
+                imiButton: "",
+                imiClass: "d-none imi",
+                prevDis: true,
+                nextDis: false,
+            }
         }
 
 
---
+++ src/chapters/unit3_2_sankou.js
@@ -0,0 +1,7 @@
+const unit3_2_sankou = {
+    tango: {
+
+    },
+}
+
+export default unit3_2_sankou;
