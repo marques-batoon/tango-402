9ef429deb47633569402a392ecb9979cdca939bb
Marques Batoon <marquesb@Marquess-MacBook-Pro.local>
2022-05-10 06:12:41 -1000

    added 4-1 tango

--- src/kanji.js
+++ src/kanji.js
@@ -343,6 +343,49 @@ const kanji = {
         背後から: { 読み方: ['はいごから'], form: '名詞、の形容詞', 意味: "from behind" },
         ばけもの: { 読み方: [''], form: '名詞', 意味: "monster ghost" },
 
+        // Unit 4_1
+        記者: { 読み方: ['きしゃ'], form: '名詞', 意味: "reporter; journalist" },
+        改姓: { 読み方: ['かいせい'], form: '名詞、する動詞、自動詞', 意味: "change of one's family name" },
+        手続き: { 読み方: ['てつづき'], form: '名詞、する動詞', 意味: "procedure; process; proceedings" },
+        役所: { 読み方: ['やくしょ'], form: '名詞', 意味: "government office; public office" },
+        職員: { 読み方: ['しょくいん'], form: '名詞', 意味: "staff member; employee" },
+        名字: { 読み方: ['みょうじ'], form: '名詞', 意味: "surname; family name" },
+        念を押す: { 読み方: ['ねんをおす'], form: '表現', 意味: "to remind (something of someone); to call attention; to make sure of; to emphasize" },
+        用紙: { 読み方: ['ようし'], form: '名詞', 意味: "blank form" },
+        震える: { 読み方: ['ふるえる'], form: '一段動詞、自動詞', 意味: "to shiver; to shake; to quake" },
+        当たり前: { 読み方: ['あたりまえ'], form: 'の形容詞、な形容詞、名詞', 意味: "obvious; natural; reasonable" },
+        違和感: { 読み方: ['いわかん'], form: '名詞', 意味: "uncomfortable feeling; feeling out of place; sense of discomfort" },
+        普段: { 読み方: ['ふだん'], form: '名詞、の形容詞、副詞', 意味: "usual; normal; usually; normal" },
+        旧姓: { 読み方: ['きゅうせい'], form: '名詞', 意味: "one's original name" },
+        運転免許証: { 読み方: ['うんてんめんきょしょう'], form: '名詞', 意味: "driver's license" },
+        窓口: { 読み方: ['まどぐち'], form: '名詞', 意味: "counter window" },
+        身分証: { 読み方: ['みぶんしょう'], form: '名詞', 意味: "ID; identification papers" },
+        会員証: { 読み方: ['かいいんしょう'], form: '名詞', 意味: "membership card" },
+        混乱: { 読み方: ['こんらん'], form: '名詞、する動詞、自動詞', 意味: "confusion; chaos; disorder" },
+        思い込み: { 読み方: ['おもいこみ'], form: '名詞', 意味: "wrong impression; assumption; prejudice" },
+        上司: { 読み方: ['じょうし'], form: '名詞', 意味: "(one's) superior; (one's) boss" },
+        報告: { 読み方: ['ほうこく'], form: '名詞、する動詞、他動詞', 意味: "report; information" },
+        勘違い: { 読み方: ['かんちがい'], form: '名詞、する動詞、自動詞', 意味: "misunderstanding; mistaken idea; wrong guess" },
+        制度: { 読み方: ['せいど'], form: '名詞、の形容詞', 意味: "system; organization" },
+        認める: { 読み方: ['みとめる'], form: '一段動詞、他動詞', 意味: "to recognize; to observe; to notice; to approve; to admit" },
+        調査: { 読み方: ['ちょうさ'], form: '名詞、する動詞、他動詞', 意味: "survey; investigation" },
+        姉妹: { 読み方: ['しまい'], form: '名詞', 意味: "sisters" },
+        求める: { 読み方: ['もとめる'], form: '一段動詞、他動詞', 意味: "to want; to wish for" },
+        長男: { 読み方: ['ちょうなん'], form: '名詞', 意味: "eldest son (may be the only son)" },
+        末: { 読み方: ['〜のすえ'], form: '副詞、名詞', 意味: "(finally)/at last) after~; at the end of" },
+        受け入れる: { 読み方: ['うけいれる'], form: '一段動詞、他動詞', 意味: "to accept; to receive; to agree" },
+        食い違い: { 読み方: ['くいちがい'], form: '名詞', 意味: "difference; disagreement; conflict; clash; inconsistency" },
+        加わる: { 読み方: ['くわわる'], form: '五段動詞、自動詞', 意味: "to be added; to join in (e.g. a group of friends); to increase; to gain in (e.g. influence); to be applied (e.g. pressure)" },
+        意見書: { 読み方: ['いけんしょ'], form: '名詞', 意味: "opinion in writing; written opinion" },
+        議会: { 読み方: ['ぎかい'], form: '名詞', 意味: "congress; parliament; diet" },
+        都合: { 読み方: ['〜のつごうで'], form: '副詞', 意味: "for reasons of~" },
+        議員: { 読み方: ['ぎいん'], form: '名詞', 意味: "member of parliament; member of the Diet" },
+        同意: { 読み方: ['どうい'], form: '名詞、する動詞、自動詞', 意味: "agreement; approval; same opinion" },
+        死亡: { 読み方: ['しぼう'], form: '名詞、する動詞、自動詞', 意味: "death; dying; mortality" },
+        国会: { 読み方: ['こっかい'], form: '名詞', 意味: "National Diet; legislative assembly of Japan" },
+
+
+
         //: { 読み方: [''], form: '', 意味: "" },
     },
     kaku: {
@@ -529,5 +572,4 @@ const kanji = {
         // : { 読み方: [''], form: '', 意味: ""  },
     }
 }
-
-export default kanji;
+export default kanji;   
